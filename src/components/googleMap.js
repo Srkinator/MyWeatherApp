955503e82e0ed08c74e169c957084f32f508afab
Srkinator <srki2207@gmail.com>
2017-12-19 23:41:04 +0100

    map works, need to make it responsive

--- src/components/googleMap.js
+++ src/components/googleMap.js
@@ -5,9 +5,9 @@ import { fetchData } from "../services/fetchData";
 
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>{
-
     
   return <GoogleMap
+    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx88sfhulbtSOSN1U3UxWL-P67PEExBiE&v=3.exp&libraries=geometry,drawing,places"
     defaultZoom={8}
     defaultCenter={{ lat: props.cityCord.lat, lng: props.cityCord.lon }}
     center={{ lat: props.cityCord.lat, lng: props.cityCord.lon }}
@@ -17,3 +17,4 @@ const MyMapComponent = withScriptjs(withGoogleMap((props) =>{
 }))
 
 export default MyMapComponent;
+
